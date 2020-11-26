0850e411750acbe16022044a573b4fc08082f6ae
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-11-26 09:30:52 +0530

    logger singleton correct implementation

--- src/appLogger.py
+++ src/appLogger.py
@@ -5,47 +5,47 @@ from logging import Logger
 import pandas as pd
 from src.appConfig import getConfig
 
-appLogger: Logger = logging.getLogger()
+class AppLogger:
+    __instance: Logger = logging.getLogger()
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if AppLogger.__instance == None:
+            raise Exception("app logger is not yet initialized")
+        return AppLogger.__instance
+
+    @staticmethod
+    def initLogger(appConfig: dict):
+        # formatting for log stash
+        logstashFormatter = LogstashFormatter(
+            message_type='python-logstash',
+            extra=dict(application='mis_outages_service'))
+
+        # set app logger name and minimum logging level
+        appLogger = logging.getLogger('python-logstash-logger')
+        appLogger.setLevel(logging.INFO)
+
+        # configure console logging
+        streamHandler = logging.StreamHandler()
+        # streamHandler.setFormatter(logstashFormatter)
+        appLogger.addHandler(streamHandler)
+
+        # configure logstash logging
+        host = appConfig["logstash_host"]
+        port = appConfig["logstash_port"]
+        if not(pd.isna(host)) and not(pd.isna(port)):
+            logstashHandler = AsynchronousLogstashHandler(
+                host, port, database_path='logstash.db')
+            logstashHandler.setFormatter(logstashFormatter)
+            appLogger.addHandler(logstashHandler)
+        AppLogger.__instance = appLogger
 
 
 def initAppLogger(appConfig: dict) -> Logger:
-    """initialize logger
-
-    Args:
-        appConfig (dict): application config dictionary
-
-    Returns:
-        Logger: logger object
-    """
-    # formatting for log stash
-    logstashFormatter = LogstashFormatter(
-        message_type='python-logstash',
-        extra=dict(application='mis_outages_service'))
-
-    # set app logger name and minimum logging level
-    appLogger = logging.getLogger('python-logstash-logger')
-    appLogger.setLevel(logging.INFO)
-
-    # configure console logging
-    streamHandler = logging.StreamHandler()
-    # streamHandler.setFormatter(logstashFormatter)
-    appLogger.addHandler(streamHandler)
-
-    # configure logstash logging
-    host = appConfig["logstash_host"]
-    port = appConfig["logstash_port"]
-    if not(pd.isna(host)) and not(pd.isna(port)):
-        logstashHandler = AsynchronousLogstashHandler(
-            host, port, database_path='logstash.db')
-        logstashHandler.setFormatter(logstashFormatter)
-        appLogger.addHandler(logstashHandler)
-    return appLogger
+    AppLogger.initLogger(appConfig)
+    return AppLogger.getInstance()
 
 
 def getAppLogger() -> Logger:
-    """get the logger object for logging
-
-    Returns:
-        Logger: logger object
-    """
-    return appLogger
+    return AppLogger.getInstance()
---
+++ tests/test_appLogger.py
@@ -0,0 +1,14 @@
+import unittest
+import datetime as dt
+from src.appConfig import getConfig
+from src.appLogger import initAppLogger
+
+
+class TestAppLogger(unittest.TestCase):
+    def test_run(self) -> None:
+        """tests the function that Logs data
+        """
+        appConfig = getConfig()
+        appLogger = initAppLogger(appConfig)
+        appLogger.info("test logging", extra={"ctx": "testing"})
+        self.assertTrue(True)
